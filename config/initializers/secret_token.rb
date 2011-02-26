b4e5dbea0a86134fd3d19c1a8a5a560d7a5bd222
Iqbal Farabi <farabi.iqbal@gmail.com>
2011-02-26 22:18:57 +0700

    adding new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spoor::Application.config.secret_token = '3ae354440093e5d9beb89fd6696a1473661f2d02af79fd19fb0354378106ad9f261dc70e0e60bbddcfdec71d9ae6414435a314b9fdb30a5c20ce475be939d7cf'
